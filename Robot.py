d09134db23916f2feb363adf03c6ce1f718f8d48
agreenfield25 <agreenfield25@choate.edu>
2023-04-26 10:29:16 -0400

    Need To add drivestraight method so a lot of errors

--- Robot.py
+++ Robot.py
@@ -2,25 +2,30 @@ import wpilib
 
 from wpilib import TimedRobot
 from robotcontainer import RobotContainer
+from drivetrain import Drivetrain
 
 class MyRobot(TimedRobot):
 
     def robotInit(self):
-        self.container = RobotContaier()
+        self.container = RobotContainer()
 
     def autonomousInit(self) -> None:
-
-        pass
+        self.auto=self.container.get_aitonomous()
 
     def autonomousPeriodic(self) -> None:
+        self.auto.run()
 
-        pass
 
     def teleopInit(self) -> None:
-        pass
+
+        ...
 
     def teleopPeriodic(self) -> None:
-        pass
+      forward = self.container.controller.getRawAxis(0)
+      rotate = self.container.controller.getRawAxis(1)
+      self.container.drivetrain.arcadeDrive(rotate,forward)
+      print(f"Forward:{forward} Rotate: {rotate}")
+
 
 
 
