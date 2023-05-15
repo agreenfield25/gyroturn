1242e40dd401443bbbe1b71614272874cbd7e4a2
agreenfield25 <agreenfield25@choate.edu>
2023-05-15 12:23:16 -0400

    Tests

--- tests/drivetrain_test.py
+++ tests/drivetrain_test.py
@@ -27,7 +27,7 @@ def test_arcadeDrive(drivetrain: Drivetrain) ->None:
         drivetrain.drive.arcadeDrive(0.2,0.3)
 
         #Assert
-        arcadeDrive.assert_called_once()
+        arcadeDrive.assert_called_once_with(0.2,0.3)
 
 def test_resetencoders(drivetrain: Drivetrain)->None:
         #setup
@@ -43,18 +43,28 @@ def test_resetencoders(drivetrain: Drivetrain)->None:
         leftreset.assert_called_once()
         rightreset.assert_called_once()
 
-
-def test_averageDistanceMeter(drivetrain: Drivetrain, monkeypatch: MonkeyPatch):
+@pytest.mark.parametrize(('left_distance', 'right_distance', 'output'),(
+        (2,3,2.5),
+        (5,8,6.5),
+        (4,0,2),
+        (3,3,3),
+        (-2,1,-0.5)
+))
+def test_averageDistanceMeter(drivetrain: Drivetrain, monkeypatch: MonkeyPatch,\
+                              left_distance,right_distance,output):
         #setup
 
         def mock_getRightDistanceMeter(self):
-                return 2.0
+                return right_distance
         def mock_getleftDistanceMeter(self):
-                return 3.0
+                return left_distance
 
         monkeypatch.setattr(Drivetrain,"getLeftDistanceMeter",mock_getleftDistanceMeter)
-        monkeypatch.setattr(Drivetrain, "getrightDistanceMeter", mock_getRightDistanceMeter)
+        monkeypatch.setattr(Drivetrain, "getRightDistanceMeter", mock_getRightDistanceMeter)
         #Action
         dist=drivetrain.averageDistanceMeter()
+
+
+        assert dist(2.0+3.0)/2
 
 
